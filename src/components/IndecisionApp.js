d61d292827869eeec775b68f850f088351a616c4
vickyhovey <vickyhovey@gmail.com>
2018-06-07 22:44:04 -0400

    theming with variables

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -63,26 +63,14 @@ export default class IndecisionApp extends React.Component {
   render() {
     const subtitle = 'Put your life in the hands of a computer';
 
-    return (
-      <div>
+    return <div>
         <Header subtitle={subtitle} />
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption
-          handleAddOption={this.handleAddOption}
-        />
-        <OptionModal
-          selectedOption={this.state.selectedOption}
-          handleClearSelectedOption={this.handleClearSelectedOption}
-        />
-      </div>
-    );
+        <div className="container">
+          <Action hasOptions={this.state.options.length > 0} handlePick={this.handlePick} />
+          <Options options={this.state.options} handleDeleteOptions={this.handleDeleteOptions} handleDeleteOption={this.handleDeleteOption} />
+          <AddOption handleAddOption={this.handleAddOption} />
+        </div>
+        <OptionModal selectedOption={this.state.selectedOption} handleClearSelectedOption={this.handleClearSelectedOption} />
+      </div>;
   }
 }
